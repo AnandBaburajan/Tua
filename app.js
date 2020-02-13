d432ea531d4d9a40acdb4ebfe354dfcec734fb04
Anand Baburajan <anandbaburajan@gmail.com>
2020-02-14 01:09:32 +0530

    API with display and create

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
+var fs = require('fs');
 dotenv.config();
 
 const PORT = process.env.PORT;
 
-hehe = {}
+var myRawData = fs.readFileSync('data.json');
+var myData = JSON.parse(myRawData);
 
 app.get('/api',(req,res) => {
-    res.send(hehe);
+    res.send(myData);
 });
 
 app.get('/api/add/:name/:link',(req,res) => {
-    var id=Object.keys(hehe).length + 1;
+    var id=parseInt(Object.keys(myData).length) + 1;
     var data=req.params;
     var name=data.name;
     var link=data.link;
 
-    hehe[id]={name: name, link: link};
-
+    myData[id]={name: name, link: link};
+    var newData = JSON.stringify(myData);
+    fs.writeFile('data.json', newData, function(err){if(err) throw err;})
     res.redirect('/api');
 });
 
