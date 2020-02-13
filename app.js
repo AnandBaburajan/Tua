d408b6673a7ff4f1a448df23b20c1406feb0d538
Anand Baburajan <anandbaburajan@gmail.com>
2020-02-13 21:40:44 +0530

    init react and express

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+app.get('/',(req,res) => {
+    res.send('hey');
+});
+
+app.listen(PORT, () => console.log('Server is running'));
