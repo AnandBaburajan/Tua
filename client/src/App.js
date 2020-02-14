f41ac80b0c1416006d04dfc4e520041c01c292e7
Anand Baburajan <anandbaburajan@gmail.com>
2020-02-14 20:35:09 +0530

    Dynamically load lists

--- client/src/App.js
+++ client/src/App.js
@@ -7,30 +7,26 @@ function App() {
   fetch('/view')
     .then(res => res.json())
     .then(data => setData(data));
-    var link = data["data"]==null? "" : data["data"][3]["link"];
-    var name = data["data"]==null? "" : data["data"][3]["name"];
+
+    const items = []
+    for (let i=0;i<4;i++) {
+      var link = data["data"]==null? "" : data["data"][i]["link"];
+      var name = data["data"]==null? "" : data["data"][i]["name"];
+      items.push(
+        <div>
+        <h4 className="name">{name}</h4>
+        <iframe className="playlist" title="playlist" src={"https://open.spotify.com/embed/playlist/" + link} height="300" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        </div>
+      )
+    }
+
   return (
     <div className="App">
       <div className="tuanav">
         <h3 className="tua">Tua</h3>
       </div>
       <div className="list">
-        <div>
-          <h4 className="name">{name}</h4>
-          <iframe className="playlist" title="playlist" src={"https://open.spotify.com/embed/playlist/" + link} height="300" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-        </div>
-        <div>
-          <h4 className="name">{name}</h4>
-          <iframe className="playlist" title="playlist" src={"https://open.spotify.com/embed/playlist/" + link} height="300" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-        </div>
-        <div>
-          <h4 className="name">{name}</h4>
-          <iframe className="playlist" title="playlist" src={"https://open.spotify.com/embed/playlist/" + link} height="300" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-        </div>
-        <div>
-          <h4 className="name">{name}</h4>
-          <iframe className="playlist" title="playlist" src={"https://open.spotify.com/embed/playlist/" + link} height="300" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-        </div>
+        {items}
       </div>
     </div>
   );
